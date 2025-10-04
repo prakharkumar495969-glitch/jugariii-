6986cf80d1c4e69ca45048a3df9154fd4f2010e4
prakharkumar495969-glitch <prakharkumar495969@gmail.com>
2025-10-04 12:22:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Yeh line zaroori hai. Isse Cloudflare Pages sahi se build hota hai.
-  base: './', 
+  base: './', // Deployment ke liye zaroori setting
 });
